35fe180c30b87da9d625893006b8d3156ecac01d
James O'Toole (No Moss) <30401863+jamesotoole@users.noreply.github.com>
2022-11-24 05:03:10 +0000

    Vast improvements

---
+++ nvidia2dollars.php
@@ -0,0 +1,10 @@
+<?php
+$costa = 1200;
+$costb = 3500;
+$performanceGain = 2.23;
+$costgain = $costa / $costb;
+
+if ($costgain < $performanceGain)
+    echo "Nvidia can go suck on a dead dogs nose";
+else
+    echo "I still think AMD have better architecture";
